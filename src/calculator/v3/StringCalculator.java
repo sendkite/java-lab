6694f9235099708578f2471f87d6b585f617afbe
sendkite <sendkite1@gmail.com>
2022-05-26 01:52:44 +0900

    FIX : 문자열계산기 극단적인 리펙도링

--- src/calculator/v3/StringCalculator.java
+++ src/calculator/v3/StringCalculator.java
@@ -1,19 +1,51 @@
 package calculator.v3;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
 
     public int add(String text) {
-        if (text == null || text.isEmpty()) {
+        if (isBlank(text)) {
             return 0;
-        }
-        String[] values = text.split(",");
-        return sum(values);
+        };
+
+        return sum(toInts(split(text)));
     }
 
-    private int sum(String[] values) {
+    private String[] split(String text) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String customDelimeter = m.group(1);
+            return m.group(2).split(customDelimeter);
+        }
+        return text.split(",|:");
+    }
+
+    private boolean isBlank(String text) {
+        return text == null || text.isEmpty();
+    }
+
+    private int[] toInts(String[] values) {
+        int[] numbers = new int[values.length];
+        for (int i = 0; i < values.length; i++) {
+            numbers[i] = toPositive(values[i]);
+        }
+        return numbers;
+    }
+
+    private int toPositive(String value) {
+        int number = Integer.parseInt(value);
+        if (number < 0) {
+            throw new RuntimeException();
+        }
+        return number;
+    }
+
+    private int sum(int[] numbers) {
         int sum = 0;
-        for (String value : values) {
-            sum += Integer.parseInt(value);
+        for (int number : numbers) {
+            sum += number;
         }
         return sum;
     }
--- src/calculator/v3/StringCalculatorTest.java
+++ src/calculator/v3/StringCalculatorTest.java
@@ -14,29 +14,49 @@ class StringCalculatorTest {
         cal = new StringCalculator();
     }
 
-    @DisplayName("null 아니면 빈문자")
+    @DisplayName("null 아니면 빈문자 인풋")
     @Test
     public void emptyString()  {
         assertEquals(0, cal.add(null));
         assertEquals(0, cal.add(""));
     }
 
-    @DisplayName("숫자가 하나")
+    @DisplayName("숫자 하나 연산")
     @Test
     public void addOneNumber() {
         assertEquals(1, cal.add("1"));
     }
 
-    @DisplayName("두개 숫자 쉼펴*(,) 구분자로 연산")
+    @DisplayName("숫자 두개 쉼표(,) 구분자로 연산")
     @Test
     public void add_쉼표() {
         assertEquals(3, cal.add("1,2"));
     }
 
+    @DisplayName("String[] 쪼개기 테스트")
     @Test
     public void split() {
         assertArrayEquals(new String[] {"1"}, "1".split(","));
         assertArrayEquals(new String[] {"1", "2"}, "1,2".split(","));
     }
 
+    @DisplayName("쉼표(,)와 콜론(:) 구분자 사용")
+    @Test
+    public void 쉼표_콜론() throws Exception {
+        assertEquals(6, cal.add("1,2:3"));
+    }
+
+    @DisplayName("커스텀 구분자로 연산")
+    @Test
+    public void add_custom_구분자() throws Exception {
+        assertEquals(6, cal.add("//;\n1;2;3"));
+    }
+
+    @DisplayName("음수 인풋 테스트")
+    @Test
+    public void add_negative() throws Exception {
+        assertThrows(RuntimeException.class, () -> {
+            cal.add("-1,2,3");
+        });
+    }
 }
